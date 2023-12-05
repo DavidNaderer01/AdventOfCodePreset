aa8c79873e03014fab93bea11c2696c6fea0cce9
Daffi989989 <126651209+Daffi989989@users.noreply.github.com>
2023-12-05 13:29:53 +0100

    updated to directory

--- Static/Data.cs
+++ Static/Data.cs
@@ -1,13 +1,21 @@
-﻿namespace AdventOfCodePreset.Static
+﻿using AdventOfCodePreset.Base;
+
+namespace AdventOfCodePreset.Static
 {
     public class Data
     {
-
-        public readonly string OutPath = Directory.GetCurrentDirectory() + "\\output";
-        public readonly string InPath = Directory.GetCurrentDirectory() + "\\input";
+        public readonly string OutPath = GetCurrentDirectory() + "\\output";
+        public readonly string InPath = GetCurrentDirectory() + "\\input";
         public const string OUTPUT_ENDING = "out";
         public const string INPUT_ENDING = "in";
         public const string SAMPLE_NAME = "sample";
         public const string ORIGINAL_NAME = "origin";
+
+        private static string GetCurrentDirectory()
+        {
+            DirectoryInfo currentDirectory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+
+            return currentDirectory.Parent.Parent.Parent.FullName;
+        }
     }
 }
